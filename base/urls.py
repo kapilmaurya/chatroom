5fca388308d610d3ce16c1941f034e5762cbb3d7
kapilmaurya <kapilmaurya323@gmail.com>
2022-07-02 01:30:44 +0530

    first commit

---
+++ base/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('login/',views.loginPage, name='login'),
+    path('logout/',views.logoutUser, name='logout'),
+    path('', views.home,name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('form/', views.createform, name='create-form'),
+    path('update-form/<str:pk>/', views.updateform, name='update-form'),
+    path('delete-room/<str:pk>/', views.deleteroom, name='delete-room'),
+    path('deletemessage/<str:pk>/', views.deletemessage, name='deletemssg'),
+    path('userprofile/<str:pk>/', views.UserProfile, name='user-profile'),
+    path('updateprofile/<str:pk>/', views.updateProfile, name='update-profile'),
+]
