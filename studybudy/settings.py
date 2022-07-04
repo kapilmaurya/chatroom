ade945589984c35e64eb3864bd638c849ab5ae89
kapilmaurya <kapilmaurya323@gmail.com>
2022-07-05 01:17:42 +0530

    setting configuration

--- studybudy/settings.py
+++ studybudy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uw6ida@qa3j5uz^41n=8f-6)^4=9m_&6^-c%mq%x$&@ne1-+!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://fridthapp.herokuapp.com/]
 
 
 # Application definition
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_ROOT  = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR , 'staticfiles')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
