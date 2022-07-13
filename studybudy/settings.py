82f6da2d628b8e6b8ce416fe722ac9d0704debd4
kapilmaurya <kapilmaurya323@gmail.com>
2022-07-13 18:16:10 +0530

    setting update

--- studybudy/settings.py
+++ studybudy/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uw6ida@qa3j5uz^41n=8f-6)^4=9m_&6^-c%mq%x$&@ne1-+!@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['fridthapp.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['talk2u.herokuapp.com','127.0.0.1']
 
 
 # Application definition
